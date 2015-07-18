952af64a7e23b9468620a03f982c476cb3b29663
Lucas <lucas@linux.site>
2015-07-18 21:37:23 +0200

    correction + fin des fonctions de verif absents

--- v1/fonctions/verifabsence.c
+++ v1/fonctions/verifabsence.c
@@ -1,22 +1,49 @@
-int absentligne(int k, char argv[9][9], int i)
+int absentligne(int k, char **tableau, int i)
 {
 	int j;
 
 	j = 0;
 	while (j < 9)
-		if(argv[i][j] == k)
+	{
+	if(tableau[i][j] == k)
 		return (0);
+		j++;
+	}
 	return (1);
 }
 
-int absentcolonne(int k, char argv[9][9], int j)
+int absentcolonne(int k, char **tableau, int j)
 {
 	int i;
 
 	i = 0;
 	while (i < 9)
-		i++;
-		if(argv[i][j] == k)
+	{
+		if(tableau[i][j] == k)
 		return (0);
+		i++;
+	}
 	return (1);
+}
+
+int absentblock(int k, char **tableau, int i, int j)
+{
+	int _i;
+	int _j;
+
+	_i = i - (i % 3);
+	_j = j - (j % 3);
+	while (i < _i + 3)
+	{
+		i = _i;
+		i++;
+		while (j < _j + 3)
+		{
+			j = _j + 3;
+			j++;
+			if (**tableau == k)
+				return (0);
+		}
+	return(1);
+	}
 }
